76f32ddff65cb949900e4a607a2b6991411eeaaa
RickAstley <rickastley1811@gmail.com>
2017-08-29 12:39:35 +0900

    ArithOp.java 파일 추가

---
+++ Chapter04_Operators/ArithOp.java
@@ -0,0 +1,21 @@
+/*
+ * 파일이름: ArithOp.java
+ * 작 성 자: RickAstley
+ * 작 성 일: 2017년 8월 29일
+ * 목    적: Study Java Language
+ */
+
+public class ArithOp {
+
+	public static void main(String[] args) {
+		int num1 = 7;
+		int num2 = 3;
+		
+		System.out.println("num1 + num2 = " + (num1 + num2));
+		System.out.println("num1 - num2 = " + (num1 - num2));
+		System.out.println("num1 * num2 = " + (num1 * num2));
+		System.out.println("num1 / num2 = " + (num1 / num2));
+		System.out.println("num1 % num2 = " + (num1 % num2));
+	}
+
+}
