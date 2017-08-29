d48ae6c1ad8948b4bd94c72c47091f8201249aed
RickAstley <rickastley1811@gmail.com>
2017-08-29 12:13:51 +0900

    Boolean.java 파일 추가

---
+++ Boolean.java
@@ -0,0 +1,22 @@
+/*
+ * 파일이름: Boolean.java
+ * 작 성 자: RickAstley
+ * 작 성 일: 2017년 8월 29일
+ * 목    적: Study Java Language
+ */
+
+public class Boolean {
+
+	public static void main(String[] args) {
+		boolean b1 = true;
+		boolean b2 = false;
+		System.out.println(b1);  // b1이 지닌 값 출력
+		System.out.println(b2);
+		
+		int num1 = 10;
+		int num2 = 20;
+		System.out.println(num1 < num2);  //true
+		System.out.println(num1 > num2);  //false
+	}
+
+}
