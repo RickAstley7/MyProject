be6b0ea6e710db8dd600f77b7510d0c93c8161e9
RickAstley <rickastley1811@gmail.com>
2017-08-29 20:31:29 +0900

    PrefixOp.java 파일 추가

---
+++ Chapter04_Operators/PrefixOp.java
@@ -0,0 +1,17 @@
+/*
+ * 파일이름: ArithOp.java
+ * 작 성 자: RickAstley
+ * 작 성 일: 2017년 8월 29일
+ * 목    적: Study Java Language
+ */
+
+public class PrefixOp {
+
+	public static void main(String[] args) {
+		int num = 7;
+		System.out.println(++num); // num의 값 하나 증가 후 출력
+		System.out.println(++num); // num의 값 하나 증가 후 출력
+		System.out.println(num);
+	}
+
+}
