faf44b8e7bb73ebad9c56b3467208a0e6024bfbd
RickAstley <rickastley1811@gmail.com>
2017-08-29 11:52:50 +0900

    SystemOutPrintln.java 파일 추가

---
+++ SystemOutPrintln.java
@@ -0,0 +1,13 @@
+public class SystemOutPrintln {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(8);
+		System.out.println(3.14);
+		System.out.println("3 + 4 = " + 7);
+		System.out.println(3.14 + "는 실수입니다.");
+		System.out.println("3 + 4" + " 의 연산 결과는 7입니다.");
+		System.out.println(3 + 4);
+	}
+	
+}
