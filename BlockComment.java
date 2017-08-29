d31dc9a3c5605441e3e89a6f6a0c4ed9b8320d9b
RickAstley <rickastley1811@gmail.com>
2017-08-29 11:59:47 +0900

    BlockComment.java 파일 추가

---
+++ BlockComment.java
@@ -0,0 +1,19 @@
+/*
+ * 파일이름: BlockComment.java
+ * 작 성 자: RickAstley
+ * 작 성 일: 2017년 8월 29일
+ * 목    적: System.out.println 메소드의 기능 테스트
+ */
+
+public class BlockComment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 다음은 단순한 정수의 출력
+		System.out.println(7);
+		
+		/* 다음은 덧셈 결과의 출력 */
+		System.out.println(12+1);
+	}
+
+}
