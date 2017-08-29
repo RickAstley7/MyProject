06512a09758ea0dd29c234bef352bb6a6ed64098
RickAstley <rickastley1811@gmail.com>
2017-08-29 11:05:15 +0900

    FirstJavaProgram.java 파일 추가

---
+++ FirstJavaProgram.java
@@ -0,0 +1,10 @@
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Java");
+		System.out.println("First Java program");
+	}
+}
