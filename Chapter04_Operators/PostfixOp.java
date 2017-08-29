489630f3ef3b7517fbc2e205b38b9531b1da80a0
RickAstley <rickastley1811@gmail.com>
2017-08-29 20:34:38 +0900

    PostfixOp.java 파일 추가

---
+++ Chapter04_Operators/PostfixOp.java
@@ -0,0 +1,21 @@
+/*
+ * 파일이름: ArithOp.java
+ * 작 성 자: RickAstley
+ * 작 성 일: 2017년 8월 29일
+ * 목    적: Study Java Language
+ */
+
+public class PostfixOp {
+
+	public static void main(String[] args) {
+		int num = 5;
+		System.out.println((num++) + " "); // 출력 후에 값이 증가
+		System.out.println((num++) + " "); // 출력 후에 값이 증가
+		System.out.println(num + "\n");
+		
+		System.out.println((num--) + " "); // 출력 후에 값이 감소
+		System.out.println((num--) + " "); // 출력 후에 값이 감소
+		System.out.println(num);
+	}
+
+}
