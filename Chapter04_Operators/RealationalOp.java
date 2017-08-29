f8564ee51830236039a9612f86653f2de51ce4fd
RickAstley <rickastley1811@gmail.com>
2017-08-29 13:25:28 +0900

    RelationalOp.java 파일 추가

---
+++ Chapter04_Operators/RealationalOp.java
@@ -0,0 +1,18 @@
+/*
+ * 파일이름: RelationalOp.java
+ * 작 성 자: RickAstley
+ * 작 성 일: 2017년 8월 29일
+ * 목    적: Study Java Language
+ */
+
+public class RealationalOp {
+
+	public static void main(String[] args) {
+		System.out.println("3 >= 2 : " + (3 >= 2));
+		System.out.println("3 <= 2 : " + (3 <= 2));
+		System.out.println("7.0 == 7 : " + (7.0 == 7));
+		System.out.println("7.0 != 7 : " + (7.0 != 7));
+		// 7.0 과 7은 다르지만 자동 형 변환이 일어나서 true를 반환한다
+	}
+
+}
