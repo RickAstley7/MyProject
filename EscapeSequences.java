604abf0cdb284451de67de2b1057f37213fb1cc7
RickAstley <rickastley1811@gmail.com>
2017-08-29 12:25:34 +0900

    EscapeSequences.java 파일 추가

---
+++ EscapeSequences.java
@@ -0,0 +1,17 @@
+/*
+ * 파일이름: EscapeSequences.java
+ * 작 성 자: RickAstley
+ * 작 성 일: 2017년 8월 29일
+ * 목    적: Study Java Language
+ */
+
+public class EscapeSequences {
+
+	public static void main(String[] args) {
+		System.out.println("AB" + '\b' + 'C');
+		System.out.println("AB" + '\t' + 'C');
+		System.out.println("AB" + '\n' + 'C');
+		System.out.println("AB" + '\r' + 'C');
+	}
+
+}
