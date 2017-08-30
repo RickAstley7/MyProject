0927a00f2a4eb61a70a538ac1c07d056797441bf
RickAstley <rickastley1811@gmail.com>
2017-08-30 19:49:48 +0900

    ForBasic.java 파일 추가

---
+++ Chapter05_Control/ForBasic.java
@@ -0,0 +1,15 @@
+package Chapter5;
+/*
+ * 파일이름: ForBasic.java
+ * 작 성 자: RickAstley
+ * 작 성 일: 2017년 8월 30일
+ * 목    적: Study Java Language
+ */
+public class ForBasic {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; i++)
+			System.out.println("I love Java" + i);
+	}
+
+}
