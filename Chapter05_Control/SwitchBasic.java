f5f16020d74b91955e81f2c550e4cee9a18ece1a
RickAstley <rickastley1811@gmail.com>
2017-08-30 19:34:39 +0900

    SwitchBasic.java 파일 수정

--- Chapter05_Control/SwitchBasic.java
+++ Chapter05_Control/SwitchBasic.java
@@ -13,10 +13,13 @@ public class SwitchBasic {
 		switch(n) {
 		case 1:
 			System.out.println("Simple Java");
+			break;
 		case 2:
 			System.out.println("Funny Java");
+			break;
 		case 3:
 			System.out.println("Fantastic Java");
+			break;
 		default:
 			System.out.println("The best programing language");
 		}
