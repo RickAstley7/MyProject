4064ebe5d744da2ec223199c8cb469d73481623a
RickAstley <rickastley1811@gmail.com>
2017-08-31 14:01:44 +0900

    DupRef.java 파일 추가

---
+++ Chapter07_Class/DupRef.java
@@ -0,0 +1,39 @@
+package Chapter7;
+/*
+ * 파일이름: DupRef.java
+ * 작 성 자: RickAstley
+ * 작 성 일: 2017년 8월 31일
+ * 목    적: Study Java Language
+ */
+class BankAccount {
+	int balance = 0;
+	
+	public int deposit(int amount) {
+		balance += amount;
+		return balance;
+	}
+	public int withdraw(int amount) {
+		balance -= amount;
+		return balance;
+	}
+	public int checkMyBalance() {
+		System.out.println("잔액 : " + balance);
+		return balance;
+	}
+}
+
+class DupRef {
+
+	public static void main(String[] args) {
+		BankAccount ref1 = new BankAccount();
+		BankAccount ref2 = ref1;
+		
+		ref1.deposit(3000);
+		ref2.deposit(2000);
+		ref1.withdraw(400);
+		ref2.withdraw(300);
+		ref1.checkMyBalance();
+		ref2.checkMyBalance();
+	}
+
+}
