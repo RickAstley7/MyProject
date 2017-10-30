560b26770403fd2cc2091377504905cfa193a943
RickAstley <rickastley1811@gmail.com>
2017-10-30 15:21:36 +0900

    Calculator 만들기

---
+++ HelloCSharpWin/HelloCSharpWin/Calc.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace HelloCSharpWin
+{
+    partial class Calc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HelloLabel = new System.Windows.Forms.Label();
+            this.Sum1 = new System.Windows.Forms.TextBox();
+            this.Sum2 = new System.Windows.Forms.TextBox();
+            this.SumNumbers = new System.Windows.Forms.Button();
+            this.SumResult = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // HelloLabel
+            // 
+            this.HelloLabel.AutoSize = true;
+            this.HelloLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.HelloLabel.Location = new System.Drawing.Point(31, 34);
+            this.HelloLabel.Name = "HelloLabel";
+            this.HelloLabel.Size = new System.Drawing.Size(105, 12);
+            this.HelloLabel.TabIndex = 0;
+            this.HelloLabel.Text = "여기를 클릭하세요";
+            this.HelloLabel.Click += new System.EventHandler(this.HelloLabel_Click);
+            // 
+            // Sum1
+            // 
+            this.Sum1.Location = new System.Drawing.Point(33, 103);
+            this.Sum1.Name = "Sum1";
+            this.Sum1.Size = new System.Drawing.Size(58, 21);
+            this.Sum1.TabIndex = 1;
+            // 
+            // Sum2
+            // 
+            this.Sum2.Location = new System.Drawing.Point(162, 103);
+            this.Sum2.Name = "Sum2";
+            this.Sum2.Size = new System.Drawing.Size(62, 21);
+            this.Sum2.TabIndex = 2;
+            // 
+            // SumNumbers
+            // 
+            this.SumNumbers.Location = new System.Drawing.Point(243, 103);
+            this.SumNumbers.Name = "SumNumbers";
+            this.SumNumbers.Size = new System.Drawing.Size(75, 23);
+            this.SumNumbers.TabIndex = 3;
+            this.SumNumbers.Text = "=";
+            this.SumNumbers.UseVisualStyleBackColor = true;
+            this.SumNumbers.Click += new System.EventHandler(this.SumNumbers_Click);
+            // 
+            // SumResult
+            // 
+            this.SumResult.Location = new System.Drawing.Point(334, 103);
+            this.SumResult.Name = "SumResult";
+            this.SumResult.Size = new System.Drawing.Size(73, 21);
+            this.SumResult.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(116, 101);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(31, 23);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Calc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 320);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.SumResult);
+            this.Controls.Add(this.SumNumbers);
+            this.Controls.Add(this.Sum2);
+            this.Controls.Add(this.Sum1);
+            this.Controls.Add(this.HelloLabel);
+            this.Name = "Calc";
+            this.Text = "계산기";
+            this.Load += new System.EventHandler(this.Calc_Load);
+            this.Click += new System.EventHandler(this.HelloLabel_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HelloLabel;
+        private System.Windows.Forms.TextBox Sum1;
+        private System.Windows.Forms.TextBox Sum2;
+        private System.Windows.Forms.Button SumNumbers;
+        private System.Windows.Forms.TextBox SumResult;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ HelloCSharpWin/HelloCSharpWin/Calc.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloCSharpWin
+{
+    public partial class Calc : Form
+    {
+        public Calc()
+        {
+            InitializeComponent();
+        }
+        public static int op = 0;
+
+        private void HelloLabel_Click(object sender, EventArgs e)
+        {
+            HelloLabel.Text = "Hello C#";
+        }
+
+        private void SumNumbers_Click(object sender, EventArgs e)
+        {
+            int numChk = 0;
+            if (String.IsNullOrWhiteSpace(Sum1.Text))
+            {
+                MessageBox.Show("첫번째 숫자를 입력해 주세요");
+                Sum1.Focus();
+                return;
+            }
+            if (!int.TryParse(Sum1.Text, out numChk))
+            {
+                MessageBox.Show("올바른 첫번째 숫자를 입력해 주세요");
+                Sum1.SelectAll();
+                Sum1.Focus();
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(Sum2.Text))
+            {
+                MessageBox.Show("두번째 숫자를 입력해 주세요");
+                Sum2.Focus();
+                return;
+            }
+            else if (!int.TryParse(Sum2.Text, out numChk)) {
+                MessageBox.Show("올바른 두번째 숫자를 입력해 주세요");
+                Sum2.SelectAll();
+                Sum2.Focus();
+                return;
+            }
+            else if (op == 0)
+            {
+                MessageBox.Show("연산자를 선택해 주세요");
+                btnAdd.Focus();
+                return;
+            }
+            else
+            {
+                int num1 = Convert.ToInt32(Sum1.Text);
+                int num2 = Convert.ToInt32(Sum2.Text);
+                int sum = 0;
+
+                switch (op)
+                {
+                    case 1:
+                        sum = Add(num1, num2);
+                        break;
+                    case 2: break;
+                    case 3: break;
+                    case 4: break;
+                    default:
+                        break;
+                }
+
+                SumResult.Text = sum.ToString();
+                op = 0;
+            }
+        }
+
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Mul(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public int Div(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            op = 1;
+        }
+
+        private void Calc_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
